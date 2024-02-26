c5ff468e55345f02545a4e61b480f2f611155412
Huzaifa <huzaifanaeem909@gmail.com>
2024-02-27 00:05:16 +0500

    add file

---
+++ 1_Basics/2_escape_sequence.py
@@ -0,0 +1,8 @@
+# Escape sequence are characters and never get printed as a part of output.
+
+print("It's alright.")  # \'	Single Quote
+print('He said,"Hello World"')  # \"	Double Quote
+print("This will insert one \\ (backslash).")  # \\	Backslash
+print("Hello\nWorld!")  # \n	New Line
+print("Hello\tWorld!")  # \t	Tab
+print("Hello \bWorld!")  # \b	Backspace (It erase one character)
