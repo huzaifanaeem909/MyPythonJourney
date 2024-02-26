4659e520ed04897277feeeace055def22865d482
Huzaifa <huzaifanaeem909@gmail.com>
2024-02-27 00:12:53 +0500

    deleted

--- Basics/2. escape_sequence.py
+++
@@ -1,8 +0,0 @@
-# Escape sequence are characters and never get printed as a part of output.
-
-print("It's alright.")  # \'	Single Quote
-print('He said,"Hello World"')  # \"	Double Quote
-print("This will insert one \\ (backslash).")  # \\	Backslash
-print("Hello\nWorld!")  # \n	New Line
-print("Hello\tWorld!")  # \t	Tab
-print("Hello \bWorld!")  # \b	Backspace (It erase one character)
