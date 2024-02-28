ba54a9a98f49e227e49b3eea88a7f784b0c59b2b
Huzaifa <huzaifanaeem909@gmail.com>
2024-02-28 23:10:29 +0500

    New Files Added

---
+++ 2_Operators/2_assignment.py
@@ -0,0 +1,16 @@
+""" Assignment operators are used to assign values to variables. """
+
+#  Operator     Example      Same As
+#     =	        x = 5	      x = 5
+#     +=	    x += 3	    x = x + 3
+#     -=	    x -= 3	    x = x - 3
+#     *=	    x *= 3	    x = x * 3
+#     /=	    x /= 3	    x = x / 3
+#     %=	    x %= 3	    x = x % 3
+#     //=	    x //= 3 	x = x // 3
+#     **=	    x **= 3 	x = x ** 3
+#     &=	    x &= 3	    x = x & 3
+#     |=	    x |= 3	    x = x | 3
+#     ^=	    x ^= 3	    x = x ^ 3
+#     >>=	    x >>= 3	    x = x >> 3
+#     <<=	    x <<= 3	    x = x << 3
