3f4b9de5916326b4a86f2132ba3727c50cf1269e
Huzaifa <huzaifanaeem909@gmail.com>
2024-02-29 20:34:51 +0500

    New Files Added

---
+++ 2_Operators/3_comparison.py
@@ -0,0 +1,9 @@
+""" Comparison operators are used to compare two values. """
+
+# Operator	    Name	            Example
+#    ==	        Equal	            x == y
+#    !=	      Not equal             x != y
+#    >	    Greater than            x > y
+#    <	      Less than	            x < y
+#    >=	 Greater than or equal to	x >= y
+#    <=  Less than or equal to	    x <= y
