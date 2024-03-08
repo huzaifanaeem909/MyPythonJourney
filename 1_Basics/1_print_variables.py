a08078312f82bc50b856afab0154d4a2adf123df
Huzaifa <huzaifanaeem909@gmail.com>
2024-03-08 18:57:57 +0500

    different ways to print variables

---
+++ 1_Basics/1_print_variables.py
@@ -0,0 +1,12 @@
+# Method 1
+x = 5
+y = "John"
+print(x)
+print(y)
+print("Hello, World!")
+print("Python is Fun!")
+
+# Method 2
+name = "M. Huzaifa"
+age = 20
+print(f"My name is {name} and My age is {age}")
