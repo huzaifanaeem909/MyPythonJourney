815791b3c7bbe74a08b44761700c066353bad95d
Huzaifa <huzaifanaeem909@gmail.com>
2024-03-19 22:47:20 +0500

    String Operations in Python

---
+++ 7_Strings/2_string_operations.py
@@ -0,0 +1,29 @@
+# Concatenation: Combine two or more strings using the + operator.
+first_name = "Salar"
+last_name = "Sikandar"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# Changing Case:
+text = "Hello, World!"
+print(text.lower())  # Output: hello, world!
+print(text.upper())  # Output: HELLO, WORLD!
+
+# Multiply Case:
+text = "Hello, World!"
+print(text * 3)
+
+# ASCII code:
+# The uppercase letters have ASCII values from 65 to 90, while the lowercase letters have ASCII values from 97 to 122.
+
+first_ch = "a"  # Lowercase letter ‘a’ has an ASCII value of 97.
+second_ch = "A"  # Uppercase letter ‘A’ has an ASCII value of 65.
+print(first_ch > second_ch)
+
+# If you need to check ASCII code
+ch = "D"
+print(ord(ch))
+
+# If you need to convert number to string
+num = 82
+print(chr(num))
