d25dd7a9498005021110100df696febf36c40ea0
Muhammad Huzaifa <149817734+huzaifanaeem909@users.noreply.github.com>
2024-07-27 23:07:51 +0500

    Update Arithmetic.py

--- 2_Operators/1_arithmetic.py
+++ 2_Operators/1_arithmetic.py
@@ -1,9 +1,9 @@
 """Arithmetic operators are used with numeric values to perform common mathematical operations"""
 
-# +	Addition	     x + y
-# -	Subtraction	     x - y
-# *   Multiplication   x * y
-# /	Division	     x / y	 (Note: Division always return answwer in Float_datatype)
-# %	Modulus     	 x % y
-# ** Exponentiation	 x ** y
-# // Floor division	 x // y  (Note: Floor Division always return answwer in Int_datatype. If the answer is in decimal value, than if will be rounded off to the lower interger. Example: IF the answer is 2.8, than it return the lower integer which is 2.)
++	Addition	       x + y
+-	Subtraction	     x - y
+* Multiplication   x * y
+/	Division	       x / y	 (Note: Division always return answwer in Float_datatype)
+%	Modulus     	   x % y
+** Exponentiation	 x ** y
+// Floor division	 x // y  (Note: Floor Division always return answwer in Int_datatype. If the answer is in decimal value, than if will be rounded off to the lower interger. Example: IF the answer is 2.8, than it return the lower integer which is 2.)
