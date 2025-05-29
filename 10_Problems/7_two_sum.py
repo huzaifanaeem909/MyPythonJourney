fd792509ce04da834d54a2ba04e1f785d463a37d
Muhammad Huzaifa <149817734+huzaifanaeem909@users.noreply.github.com>
2025-05-29 18:06:47 +0500

    two sum

---
+++ 10_Problems/7_two_sum.py
@@ -0,0 +1,28 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+Example 1:
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+Explanation: nums[0] + nums[1] = 2 + 7 = 9, so the indices [0, 1] are returned.
+
+Example 2:
+Input: nums = [3, 2, 4], target = 6
+Output: [1, 2]
+Explanation: nums[1] + nums[2] = 2 + 4 = 6.
+"""
+
+def twoSum(nums, target):
+    num_map = {}
+    
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in num_map:
+            # If found, return diff index and current index
+            return [num_map[diff], i]
+        # If diff not found, add current number and its index to hash map
+        num_map[num] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums,target))
